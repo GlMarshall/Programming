cbd5de7b499d39cbdc619b055453e6c6bb6ac8e1
GlMarshall <arrowmarshalll@gmail.com>
2026-03-26 14:20:53 -0700

    Simple refactoring

--- Programming/main.cpp
+++ Programming/main.cpp
@@ -1,123 +1,172 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <climits>
 #include <iomanip>
 using namespace std;
 
+// Декларация функции чтения из файла
 double** ReadMatrixFromFile(const char* filename, int& n, int& m);
+// Декларация функции вывода матрицы
 void PrintMatrix(double** matrix, int n, int m);
+// Декларация функции главного алгоритма
 void MatrixSumm(double** matrix, int n, int m, double& minsumm, int& columnCount);
+// Декларация функции освобождения ресурсов
+void FreeMatrix(double** matrix, int n);
 
 int main() {
-	cout << "=== Working With matrix A ===" << endl;
-	int n1, m1;
-	double** matrixA = ReadMatrixFromFile("matrixA.txt", n1, m1);
+    cout << "--- Working With matrix A ---" << endl;
+    // Декларация строк и столбцов
+    int n1, m1;
+    // Получение матрицы из файла
+    double** matrixA = ReadMatrixFromFile("matrixA.txt", n1, m1);
 
-	if (matrixA) {
-		PrintMatrix(matrixA, n1, m1);
+    // Проверка ненулевой матрицы
+    if (matrixA) {
+        // Вывод матрицы
+        PrintMatrix(matrixA, n1, m1);
 
-		double minSummA;
-		int columnCountA;
+        // Декларация переменной суммы
+        double minSummA;
+        // Декларация переменной кол-ва столбцов
+        int columnCountA;
 
-		MatrixSumm(matrixA, n1, m1, minSummA, columnCountA);
+        MatrixSumm(matrixA, n1, m1, minSummA, columnCountA);
 
-		if (columnCountA > 0) {
-			cout << "\nColumn Count without negative elements: " << columnCountA << endl;
-			cout << "Minimal Summ between this columns: " << minSummA << endl;
-		}
-		else {
-			cout << "\nNo Column without negative elements." << endl;
-		}
-	}
+        // Проверка ненулевого значения
+        if (columnCountA > 0) {
+            cout << "\nColumn Count without negative elements: " << columnCountA << endl;
+            cout << "Minimal Summ between this columns: " << minSummA << endl;
+        }
+        else {
+            cout << "\nNo Column without negative elements." << endl;
+        }
+    }
+    // Освобождение ресурсов
+    FreeMatrix(matrixA, n1);
+    cout << "\n\n";
 
-	cout << "\n\n";
 
+    cout << "=== Working With matrix B ===" << endl;
+    // Декларация строк и столбцов
+    int n2, m2;
+    // Получение матрицы из файла
+    double** matrixB = ReadMatrixFromFile("matrixB.txt", n2, m2);
 
-	cout << "=== Working With matrix B ===" << endl;
-	int n2, m2;
-	double** matrixB = ReadMatrixFromFile("matrixB.txt", n2, m2);
+    // Проверка ненулевой матрицы
+    if (matrixB) {
+        // Вывод матрицы
+        PrintMatrix(matrixB, n2, m2);
 
-	if (matrixB) {
-		PrintMatrix(matrixB, n1, m1);
+        // Декларация переменной суммы
+        double minSummB;
+        // Декларация переменной кол-ва столбцов
+        int columnCountB;
 
-		double minSummB;
-		int columnCountB;
+        MatrixSumm(matrixB, n2, m2, minSummB, columnCountB);
 
-		MatrixSumm(matrixB, n1, m1, minSummB, columnCountB);
+        // ПАроверка ненулевого значения
+        if (columnCountB > 0) {
+            cout << "\nColumn Count without negative elements: " << columnCountB << endl;
+            cout << "Minimal Summ between this columns: " << minSummB << endl;
+        }
+        else {
+            cout << "\nNo Column without negative elements." << endl;
+        }
+    }
+    // Освобождение ресурсов
+    FreeMatrix(matrixB, n2);
 
-		if (columnCountB > 0) {
-			cout << "\nColumn Count without negative elements: " << columnCountB << endl;
-			cout << "Minimal Summ between this columns: " << minSummB << endl;
-		}
-		else {
-			cout << "\nNo Column without negative elements." << endl;
-		}
-	}
-	
-	return 0;
+    return 0;
 }
 
+// Чтоение из файла
 double** ReadMatrixFromFile(const char* filename, int& n, int& m)
 {
-	ifstream file(filename);
+    // сам файл
+    ifstream file(filename);
 
-	if (!file.is_open()) {
-		cerr << "Error, can't open file" << endl;
-		return nullptr;
-	}
+    // Проверка открытости файла
+    if (!file.is_open()) {
+        cout << "Error, can't open file" << endl;
+        return nullptr;
+    }
 
-	file >> n >> m;
+    // Записываем в переменые значения кол-ва сткрок и столбцов
+    file >> n >> m;
 
-	double** matrix = new double* [n];
-	for (int i = 0; i < n; i++) {
-		matrix[i] = new double[m];
-	}
+    // создаем матрицу  
+    double** matrix = new double* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new double[m];
+    }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			file >> matrix[i][j];
-		}
-	}
-	file.close();
+    // Заполняем матрицу
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            file >> matrix[i][j];
+        }
+    }
+    // Закрываем поток
+    file.close();
 
-	return matrix;
+    // Возвращаем матрицу
+    return matrix;
 }
 
+// Функция вывода матрицы
 void PrintMatrix(double** matrix, int n, int m)
 {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cout << setw(10) << fixed << setprecision(2) <<matrix[i][j];
-		}
-		cout << endl;
-	}
+    // Столбцы
+    for (int i = 0; i < n; i++)
+    {
+        // Строки
+        for (int j = 0; j < m; j++)
+        {
+            // Вывод
+            cout << setw(10) << fixed << setprecision(2) << matrix[i][j];
+        }
+        // Новая строка
+        cout << endl;
+    }
 }
 
+// Функция основного алгоритма
 void MatrixSumm(double** matrix, int n, int m, double& minSumm, int& columnCount)
 {
-	minSumm = DBL_MAX;
-	columnCount = 0;
+    // Инициализация начальных значений
+    minSumm = DBL_MAX;
+    columnCount = 0;
 
-	for (int i = 0; i < m; i++)
-	{
-		bool hasNegative = false;
-		double columnSumm = 0.0;
-		for (int j = 0; j < n; j++)
-		{
-			if (matrix[i][j] < 0) {
-				hasNegative = true;
-				break;
-			}
+    // Столбцы
+    for (int col = 0; col < m; col++) {
+        // Наличие отрицательного
+        bool hasNegative = false;
+        // Сумма
+        double colSum = 0.0;
+        // Строки
+        for (int row = 0; row < n; row++) {
+            // Проверка отрицательного
+            if (matrix[row][col] < 0) {
+                hasNegative = true;
+                break;
+            }
+            // Суммирование
+            colSum += matrix[row][col];
+        }
+        // Присвоенние минимальной суммы
+        if (!hasNegative) {
+            columnCount++;
+            if (colSum < minSumm) minSumm = colSum;
+        }
+    }
+}
 
-			columnSumm += matrix[i][j];
-		}
-
-		if (!hasNegative) {
-			columnCount += 1;
-			if (columnSumm < minSumm)
-				minSumm = columnSumm;
-		}
-	}
+// Функция освобождения ресурсов (Пояснять не надо)
+void FreeMatrix(double** matrix, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 }
